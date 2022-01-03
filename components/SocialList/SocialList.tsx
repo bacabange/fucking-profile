1cbfd3e72840f73cc6f2931d24c1e96f5796258b
Stiven <stivencastillo.90@gmail.com>
2022-01-03 12:18:39 -0500

    Change social links

--- components/SocialList/SocialList.tsx
+++ components/SocialList/SocialList.tsx
@@ -8,16 +8,16 @@ interface SocialListProps {
 const SocialList: React.FC<SocialListProps> = ({ hide }) => {
   return (
     <div className={`flex flex-row justify-around space-x-3 lg:justify-start ease-out duration-300 ${hide && 'hidden'}`}>
-      <a href="https://twitter.com/bacabange" target="_blank" rel="noreferrer">
+      <a href="https://twitter.com/stivncastillo_" target="_blank" rel="noreferrer">
         <TwitterIcon width={20} height={20} fillColor="fill-slate-400 hover:fill-blue-400" />
       </a>
       <a href="https://linkedin.com/in/stivncastillo/" target="_blank" rel="noreferrer">
         <LinkedinIcon width={20} height={20} fillColor="fill-slate-400 hover:fill-blue-400" />
       </a>
-      <a href="https://github.com/bacabange" target="_blank" rel="noreferrer">
+      <a href="https://github.com/stivncastillo" target="_blank" rel="noreferrer">
         <GithubIcon width={20} height={20} fillColor="fill-slate-400 hover:fill-blue-400" />
       </a>
-      <a href="https://dribbble.com/bacabange" target="_blank" rel="noreferrer">
+      <a href="https://dribbble.com/stivncastillo" target="_blank" rel="noreferrer">
         <DribbbleIcon width={20} height={20} fillColor="fill-slate-400 hover:fill-blue-400" />
       </a>
       <a href="mailto:stivencastillo.90@gmail.com" target="_blank" rel="noreferrer">
